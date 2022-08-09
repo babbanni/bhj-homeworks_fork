a2909f27bd720a71b941ef68d9b0db132f797516
babbanni <vlad_paltsev@bk.ru>
2022-08-09 18:25:13 +0300

    mole-game task.js edited

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,18 +2,237 @@ let dead = document.getElementById('dead');
 let deadCount = dead.textContent;
 let lost = document.getElementById('lost');
 let lostCount = lost.textContent;
-let hole = getHole(1);
 
-hole.onclick = function() {
-    if (hole.className.includes('hole_has-mole')) {
-        deadCount++;
+
+getHole(1).onclick = function() {
+    if (getHole(1).className.includes('hole_has-mole')) {
+        dead.textContent = deadCount++;
     } else {
-        lostCount++;
+        lost.textContent = lostCount++;
+    }
+    if (lost.textContent == 5) {
+        alert('Поражение!');
+    } else if (dead.textContent == 10) {
+        alert('Вы победили!');
+    }
+
+    if (lost.textContent == 5) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    } else if (dead.textContent == 10) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
     }
 }
 
-if (lostCount == 5) {
-    alert('Поражение!');
-} else if (deadCount == 10) {
-    alert('Вы победили!');
+getHole(2).onclick = function() {
+    if (getHole(2).className.includes('hole_has-mole')) {
+        dead.textContent = deadCount++;
+    } else {
+        lost.textContent = lostCount++;
+    }
+
+    if (lost.textContent == 5) {
+        alert('Поражение!');
+    } else if (dead.textContent == 10) {
+        alert('Вы победили!');
+    }
+
+    if (lost.textContent == 5) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    } else if (dead.textContent == 10) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    }
+}
+
+getHole(3).onclick = function() {
+    if (getHole(3).className.includes('hole_has-mole')) {
+        dead.textContent = deadCount++;
+    } else {
+        lost.textContent = lostCount++;
+    }
+
+    if (lost.textContent == 5) {
+        alert('Поражение!');
+    } else if (dead.textContent == 10) {
+        alert('Вы победили!');
+    }
+
+    if (lost.textContent == 5) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    } else if (dead.textContent == 10) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    }
+}
+
+getHole(4).onclick = function() {
+    if (getHole(4).className.includes('hole_has-mole')) {
+        dead.textContent = deadCount++;
+    } else {
+        lost.textContent = lostCount++;
+    }
+
+    if (lost.textContent == 5) {
+        alert('Поражение!');
+    } else if (dead.textContent == 10) {
+        alert('Вы победили!');
+    }
+
+    if (lost.textContent == 5) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    } else if (dead.textContent == 10) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    }
+}
+
+getHole(5).onclick = function() {
+    if (getHole(5).className.includes('hole_has-mole')) {
+        dead.textContent = deadCount++;
+    } else {
+        lost.textContent = lostCount++;
+    }
+
+    if (lost.textContent == 5) {
+        alert('Поражение!');
+    } else if (dead.textContent == 10) {
+        alert('Вы победили!');
+    }
+
+    if (lost.textContent == 5) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    } else if (dead.textContent == 10) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    }
+}
+
+getHole(6).onclick = function() {
+    if (getHole(6).className.includes('hole_has-mole')) {
+        dead.textContent = deadCount++;
+    } else {
+        lost.textContent = lostCount++;
+    }
+
+    if (lost.textContent == 5) {
+        alert('Поражение!');
+    } else if (dead.textContent == 10) {
+        alert('Вы победили!');
+    }
+
+    if (lost.textContent == 5) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    } else if (dead.textContent == 10) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    }
+}
+
+getHole(7).onclick = function() {
+    if (getHole(7).className.includes('hole_has-mole')) {
+        dead.textContent = deadCount++;
+    } else {
+        lost.textContent = lostCount++;
+    }
+
+    if (lost.textContent == 5) {
+        alert('Поражение!');
+    } else if (dead.textContent == 10) {
+        alert('Вы победили!');
+    }
+
+    if (lost.textContent == 5) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    } else if (dead.textContent == 10) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    }
+}
+
+getHole(8).onclick = function() {
+    if (getHole(8).className.includes('hole_has-mole')) {
+        dead.textContent = deadCount++;
+    } else {
+        lost.textContent = lostCount++;
+    }
+
+    if (lost.textContent == 5) {
+        alert('Поражение!');
+    } else if (dead.textContent == 10) {
+        alert('Вы победили!');
+    }
+
+    if (lost.textContent == 5) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    } else if (dead.textContent == 10) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    }
+}
+
+getHole(9).onclick = function() {
+    if (getHole(9).className.includes('hole_has-mole')) {
+        dead.textContent = deadCount++;
+    } else {
+        lost.textContent = lostCount++;
+    }
+
+    if (lost.textContent == 5) {
+        alert('Поражение!');
+    } else if (dead.textContent == 10) {
+        alert('Вы победили!');
+    }
+
+    if (lost.textContent == 5) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    } else if (dead.textContent == 10) {
+        deadCount = 0;
+        dead.textContent = deadCount;
+        lostCount = 0;
+        lost.textContent = lostCount;
+    }
 }
