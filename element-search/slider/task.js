e3c85d7a8b357e66766b6e863b9a32b434522fd5
babbanni <vlad_paltsev@bk.ru>
2022-08-24 18:34:35 +0300

    slider task.js edited

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,26 +1,32 @@
 let btnLeft = document.querySelector('.slider__arrow_prev');
 let btnRight = document.querySelector('.slider__arrow_next');
 let sliderItemsArr = Array.from(document.querySelectorAll('.slider__item'));
-let activeSlide = 0;
+let activeSlide = element => element.className === 'slider__item slider__item_active';
+
+
+
+
 
 btnRight.onclick = function() {
-    if (activeSlide === sliderItemsArr.length - 1) {
-        activeSlide = 0;
+    let activeSlideIndex = sliderItemsArr.findIndex(activeSlide);
+    if (activeSlideIndex === sliderItemsArr.length - 1) {
+        activeSlideIndex = 0;
         sliderItemsArr[sliderItemsArr.length - 1].className = 'slider__item';
     } else {
-        activeSlide++;
-        sliderItemsArr[activeSlide - 1].className = 'slider__item';
+        activeSlideIndex++;
+        sliderItemsArr[activeSlideIndex - 1].className = 'slider__item';
     }
-    sliderItemsArr[activeSlide].className = 'slider__item slider__item_active';
+    sliderItemsArr[activeSlideIndex].className = 'slider__item slider__item_active';
 }
 
 btnLeft.onclick = function() {
-    if (activeSlide === 0) {
-        activeSlide = sliderItemsArr.length - 1;
+    let activeSlideIndex = sliderItemsArr.findIndex(activeSlide);
+    if (activeSlideIndex === 0) {
+        activeSlideIndex = sliderItemsArr.length - 1;
         sliderItemsArr[0].className = 'slider__item';
     } else {
-        activeSlide--;
-        sliderItemsArr[activeSlide + 1].className = 'slider__item';
+        activeSlideIndex--;
+        sliderItemsArr[activeSlideIndex + 1].className = 'slider__item';
     }
-    sliderItemsArr[activeSlide].className = 'slider__item slider__item_active';
+    sliderItemsArr[activeSlideIndex].className = 'slider__item slider__item_active';
 }
