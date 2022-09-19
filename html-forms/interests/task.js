9b0196ac4714acd82bdfb7042e011efef20731c4
babbanni <vlad_paltsev@bk.ru>
2022-09-19 21:01:51 +0300

    interests task.js edited

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,2 +1,16 @@
-const interestCheck = document.querySelectorAll('.interest__check');
-const interesCheckActive = Array.from(document.querySelectorAll('.interests_active'));
+const interests = document.querySelectorAll('.interests');
+
+interests.forEach(elem => {
+    elem.addEventListener('change', (e) => {
+        if (e.target.closest('.interests_main')) {
+            let subInterest = e.target.closest('li').querySelectorAll('.interest__check');
+            subInterest.forEach(el => {
+                if (e.target.checked) {
+                    el.checked = true;
+                } else {
+                    el.checked = false;
+                }
+            })
+        }
+    })
+})
