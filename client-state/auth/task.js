31ecf79f0ce731ed4cc517a8501a111a3f913210
babbanni <vlad_paltsev@bk.ru>
2022-10-22 20:11:45 +0300

    auth task.js edited

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,18 +1,34 @@
-const btn = document.querySelector('.btn');
-const login = document.getElementsByName('login');
-const password = document.getElementsByName('password');
+const btn = document.getElementById('signin__btn');
 const welcome = document.querySelector('.welcome');
-const xhr = new XMLHttpRequest();
-const url = 'https://netology-slow-rest.herokuapp.com/auth.php';
+const form = document.getElementById('signin__form');
+const signin = document.querySelector('.signin');
 
-btn.addEventListener('click', () => {
-    xhr.open('POST', url, true);
+if (localStorage.user_id) {
+    welcome.children.user_id.textContent = localStorage.user_id;
+    welcome.classList.add('welcome_active');
+    signin.classList.remove('signin_active');
+}
+btn.addEventListener('click', e => {
+    e.preventDefault();
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php', false);
 
-    xhr.addEventListener('readystatechange', () => {
-        if (xhr.readyState === xhr.DONE) {
+    let login = form.children[0].firstElementChild.value;
+    let password = form.children[1].firstElementChild.value;
+    let formData = new FormData;
 
-        } else {
-            alert('Неправильный пароль или логин');
-        }
-    });
+    formData.append('login', login);
+    formData.append('password', password);
+    xhr.send(formData);
+
+    let response = JSON.parse(xhr.responseText);
+
+    if (response.success) {
+        localStorage.setItem('user_id', `${response.user_id}`);
+        welcome.children.user_id.textContent = response.user_id;
+        welcome.classList.add('welcome_active');
+        signin.classList.remove('signin_active');
+    } else {
+        alert('Неверный логин/пароль');
+    }
 });
